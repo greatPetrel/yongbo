cd9232625776b9356fa3f44774abebd4b2e8146c
greatpetrel <petrel.ge@gmail.com>
2017-05-18 18:16:42 +0800

    no

---
+++ yong-park-src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Background from
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+
+};
+
+export default App;
